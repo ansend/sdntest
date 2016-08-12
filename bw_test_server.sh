39912a3de5056668f82acacf7cfa9a855803f554
ansendong <ddttcc@gmail.com>
2016-08-12 17:15:31 +0800

    minor change to tcp memory setup

--- bw_test_server.sh
+++ bw_test_server.sh
@@ -5,8 +5,6 @@ if [ $# -lt 1 ];then
                         exit
 fi
 
-
-
 tcp_mem_max=$1
 
 
@@ -16,9 +14,8 @@ echo ${tcp_mem_max}
 
 echo ${tcp_mem_default}
 
-echo ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_rmem
-echo ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_wmem
+echo 4096 ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_rmem
+echo 4096 ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_wmem
 
-nohup qperf $serverip -m 64K -t 100 tcp_bw > $log_file 2>&1 &
-
+nohup qperf $serverip -m 64K -t 100 tcp_bw > qperf_log 2>&1 &
 
