92ff9cf82129cce125ca62136184b300f3e16a13
ansendong <ddttcc@gmail.com>
2016-08-15 15:07:05 +0800

    remve meanless white space

--- bw_test_client.sh
+++ bw_test_client.sh
@@ -22,6 +22,6 @@ echo ${tcp_mem_default}
 echo ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_rmem
 echo ${tcp_mem_default} ${tcp_mem_max} > /proc/sys/net/ipv4/tcp_wmem
 
-nohup qperf $serverip -m 64K -t 100 tcp_bw > $log_file 2>&1 &
+nohup qperf $serverip -m 64K -t 100 tcp_bw >$log_file 2>&1 &
 
 
--- sdn_server_setup.sh
+++ sdn_server_setup.sh
@@ -15,12 +15,12 @@ echo "existing etcd pid" $etcdpid
 if test $( pidof 'etcd' | wc -l ) -eq 0 
         then
             #echo "./etcd -advertise-client-urls http://$eth0ip:2379,http://$eth0ip:4001 --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001"
-        nohup ./etcd -advertise-client-urls "http://$eth0ip:2379,http://$eth0ip:4001" --listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" > etcdlog 2>&1 &
+        nohup ./etcd -advertise-client-urls "http://$eth0ip:2379,http://$eth0ip:4001" --listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" >etcdlog 2>&1 &
         echo "start etcd on sdn test on server"
 else
 
         kill -9 ${etcdpid}
-        nohup ./etcd -advertise-client-urls "http://$eth0ip:2379,http://$eth0ip:4001" --listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" > etcdlog 2>&1 &
+        nohup ./etcd -advertise-client-urls "http://$eth0ip:2379,http://$eth0ip:4001" --listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" >etcdlog 2>&1 &
                 echo "restart etcd on sdn test on server"
 fi
 
@@ -32,12 +32,12 @@ flannelpid=$(pidof 'flanneld')
 
 if test $( pidof 'flanneld' | wc -l ) -eq 0 
         then
-        nohup ./flanneld -etcd-endpoints "http://$eth0ip:4001" > flannellog 2>&1 &
+        nohup ./flanneld -etcd-endpoints "http://$eth0ip:4001" >flannellog 2>&1 &
         echo "start flannel for on sdn server "
 else
             kill -2 ${flannelpid}
         kill -9 ${flannelpid}
-        nohup ./flanneld -etcd-endpoints "http://$eth0ip:4001" > flannellog 2>&1 &
+        nohup ./flanneld -etcd-endpoints "http://$eth0ip:4001" >flannellog 2>&1 &
                 echo "restart flanneld on server side"
 fi
 
@@ -46,11 +46,11 @@ qperfpid=$(pidof 'qperf')
 
 if test $( pgrep -f 'qperf' | wc -l ) -eq 0
         then
-        nohup qperf > qperflog 2>&1 &
+        nohup qperf >qperflog 2>&1 &
         echo "start qperf for on sdn server "
 else
         kill -9 ${qperfpid}
-        nohup qperf > qperflog 2>&1 &
+        nohup qperf >qperflog 2>&1 &
                 echo "restart qperf for on sdn server "
 fi
 
