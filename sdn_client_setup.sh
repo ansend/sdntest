9b2742a602466b77aa9f0e45c564c799da728669
ansendong <ddttcc@gmail.com>
2016-08-15 11:15:07 +0800

    minor change of sdn client setup

--- sdn_client_setup.sh
+++ sdn_client_setup.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-
 if [ $# -lt 1 ];then
-		echo "usage: `basename $0` etcdip"
-			exit
+        echo "usage: `basename $0` etcdip"
+            exit
 fi
 
 pid=$(pidof 'flanneld')
@@ -11,12 +10,13 @@ etcdurl=$1
 
 echo "etcd ip address" $etcdurl
 
-if test $( pgrep -f 'flanneld' | wc -l ) -eq 0 
-	then
-	./flanneld -etcd-endpoints "http://$etcdurl:4001"
-	echo "start sdn cient node for test"
+if test $( pidof 'flanneld' | wc -l ) -eq 0
+    then
+    nohup ./flanneld -etcd-endpoints "http://$etcdurl:4001" >flannel_log 2>&1 &
+    echo "start sdn client node for test" "./flanneld -etcd-endpoints http://$etcdurl:4001"
 else
-	kill -2 "$pid"
-	./flanneld -etcd-endpoints "http://$etcdurl:4001"
-fi	
-
+    kill -2 ${pid}
+    kill -9 ${pid}
+    nohup ./flanneld -etcd-endpoints "http://$etcdurl:4001" >flannel_log 2>&1 &
+    echo "restart sdn client node for test" "./flanneld -etcd-endpoints http://$etcdurl:4001"
+fi
